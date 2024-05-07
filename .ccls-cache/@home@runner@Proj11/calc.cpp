4163804f8896d4c49baa778145c1484d2a14639a
Fady Youssef <62950351+Durantulaa@users.noreply.github.com>
2024-05-07 01:43:58 +0000

    eval

--- .ccls-cache/@home@runner@Proj11/calc.cpp
+++ .ccls-cache/@home@runner@Proj11/calc.cpp
@@ -1,12 +1,3 @@
-/*
-Name: Fady Youssef
-Class: CPSC 122, Section 1
-Date Submitted: April 24, 2024
-=Description: Part 2 of Calculator Assignment 
-To Compile: make
-To Execute: ./calc "(121+12)"
-*/
-
 #include "calc.h"
 #include <cctype>  // Include cctype for isdigit and isalpha
 #include <cstdlib> // Include cstdlib for exit function
@@ -19,68 +10,70 @@ Calc::Calc(const char *argvIn) {
   stk = new Stack();
 
   int len = strlen(argvIn);
-  inFix = new char[len + 1];   // Add 1 for the null terminator
-  postFix = new char[len + 1]; // Add 1 for the null terminator
+  inFix = new char[len + 1];   // Allocate memory for the infix expression
+  postFix = new char[len + 1]; // Allocate memory for the postfix expression
 
-  strcpy(inFix, argvIn);
+  strcpy(inFix, argvIn); // Copy the input expression to the inFix member
 
+  // Check for valid tokens and balanced parentheses
   if (!CheckTokens())
-    cout << "invalid token input\n";
+    cout << "Invalid token input\n";
   else
     cout << "Tokens Balanced\n";
   if (!CheckParens())
-    cout << "invalid parens input\n";
+    cout << "Invalid parens input\n";
   else
     cout << "Parens Balanced\n";
 
-  MakeValueTbl();
-  Parse();
-  InFixToPostFix();
+  MakeValueTbl();   // Initialize the value table
+  Parse();          // Parse the infix expression
+  InFixToPostFix(); // Convert infix to postfix
 }
 
 Calc::~Calc() {
-  delete[] inFix;
-  delete[] postFix;
-  delete[] valueTbl;
+  delete[] inFix;    // Free memory allocated for infix expression
+  delete[] postFix;  // Free memory allocated for postfix expression
+  delete[] valueTbl; // Free memory allocated for value table
 }
 
 void Calc::MakeValueTbl() {
-  valueTbl = new int[26];
+  valueTbl = new int[26]; // Allocate memory for value table (26 positions)
   for (int i = 0; i < 26; i++)
-    valueTbl[i] = 0;
+    valueTbl[i] = 0; // Initialize all positions to zero
 }
 
 int Calc::FindLast(int cur) {
-
+  // Find the last digit in a digit string
   int last = cur;
   while (isdigit(inFix[last])) {
     last++;
   }
-  return last - 1; // return last digit
+  return last - 1; // Return the index of the last digit
 }
 
 bool Calc::CheckParens() {
-  bool bal = true;
+  bool bal = true; // Assume parentheses are balanced initially
   int i = 0;
   char ch = inFix[i];
   while (bal && ch != '\0') {
     if (ch == '(')
-      stk->Push(ch);
+      stk->Push(ch); // Push '(' onto the stack
     else if (ch == ')') {
       if (!stk->IsEmpty())
-        stk->Pop();
+        stk->Pop(); // Pop '(' from the stack
       else
-        bal = false;
+        bal = false; // Unbalanced parentheses
     }
     i++;
     ch = inFix[i];
   }
-  if (bal && stk->IsEmpty())
+  if (bal && stk->IsEmpty()) // Check if stack is empty (balanced)
     return true;
   return false;
 }
 
 bool Calc::CheckTokens() {
+  // Check for valid tokens in the infix expression
   for (int i = 0; inFix[i] != '\0'; i++) {
     if (!(inFix[i] == '+' || inFix[i] == '-' || inFix[i] == '*' ||
           inFix[i] == '(' || inFix[i] == ')' ||
@@ -93,39 +86,24 @@ bool Calc::CheckTokens() {
 }
 
 void Calc::Parse() {
-  MakeValueTbl(); // initialize value table
+  MakeValueTbl(); // Initialize value table
   int i = 0;
   int j = 0;
-  int last = 0;
 
+  // Parse the infix expression
   while (inFix[i] != '\0') {
-    if (inFix[i] == '(') {
-      inFix[j++] = inFix[i];
-      stk->Push(inFix[i]);
-    } else if (inFix[i] == ')') {
-      while (stk->Peek() != '(') {
-        inFix[j++] = stk->Peek();
-        stk->Pop();
-      }
-      inFix[j++] = inFix[i];
-      stk->Pop();
-    }
-
-    else if (isdigit(inFix[i])) {
-      last = FindLast(i);
+    if (isdigit(inFix[i])) {
       int num = 0;
-      for (int k = i; k <= last; k++) {
-        num = num * 10 + (inFix[k] - '0');
+      while (isdigit(inFix[i])) {
+        num = num * 10 + (inFix[i++] - '0');
       }
       inFix[j++] = 'A' + valueIdx;
-      valueTbl[valueIdx++] = num;
-      i = last;
+      valueTbl[valueIdx++] = num; // Store numeric value in value table
     } else {
-      inFix[j++] = inFix[i];
+      inFix[j++] = inFix[i++];
     }
-    i++;
   }
-  inFix[j] = '\0';
+  inFix[j] = '\0'; // Add null terminator to the parsed infix expression
 }
 
 void Calc::InFixToPostFix() {
@@ -134,20 +112,20 @@ void Calc::InFixToPostFix() {
   for (int j = 0; inFix[j] != '\0'; j++) {
     ch = inFix[j];
     if (isdigit(ch) || isalpha(ch)) {
-      postFix[i++] = ch; // Use indexing to access elements
+      postFix[i++] = ch; // Append operands to postfix expression
     } else if (ch == '(') {
-      stk->Push(ch);
+      stk->Push(ch); // Push '(' onto the stack
     } else if (ch == '+' || ch == '-' || ch == '*') {
-      stk->Push(ch);
+      stk->Push(ch); // Push operators onto the stack
     } else if (ch == ')') {
       while (!stk->IsEmpty() && stk->Peek() != '(') {
-        postFix[i++] = stk->Peek(); // Use indexing to access elements
-        stk->Pop();
+        postFix[i++] = stk->Peek(); // Append operators to postfix expression
+        stk->Pop();                 // Pop operators from the stack
       }
-      stk->Pop(); // Pop '('
+      stk->Pop(); // Pop '(' from the stack
     }
   }
-  postFix[i] = '\0'; // Add null terminator at the end
+  postFix[i] = '\0'; // Add null terminator at the end of the postfix expression
 }
 
 int Calc::Evaluate() {
@@ -158,27 +136,28 @@ int Calc::Evaluate() {
        i++) { // Loop through the postfix expression
     ch = postFix[i];
     if (isalpha(ch)) {
-      stk->Push(
-          valueTbl[ch - 'A']); // Push the value of the variable onto the stack
+      stk->Push(valueTbl[ch - 'A']); // Push operand value onto the stack
     } else if (isdigit(ch)) {
-      stk->Push(ch - '0'); // Push the digit onto the stack
+      stk->Push(ch - '0'); // Push digit onto the stack
     } else if (ch == '+' || ch == '-' || ch == '*') {
       op2 = stk->Pop();
       op1 = stk->Pop();
       switch (ch) {
       case '+':
-        stk->Push(op1 + op2);
+        stk->Push(op1 + op2); // Perform addition and push result onto the stack
         break;
       case '-':
-        stk->Push(op1 - op2);
+        stk->Push(op1 -
+                  op2); // Perform subtraction and push result onto the stack
         break;
       case '*':
-        stk->Push(op1 * op2);
+        stk->Push(op1 *
+                  op2); // Perform multiplication and push result onto the stack
         break;
       }
     }
   }
-  int result = stk->Pop(); // Return the final result
+  int result = stk->Pop(); // Pop final result from the stack
   return result;
 }
 
